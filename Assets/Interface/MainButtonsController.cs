c14d2226a4f69bc793007f51bd280fe83a9f9983
Michal Przekop <przekop.michal@gmail.com>
2018-03-25 21:13:11 +0200

    Basic UI`
    Implemented Basic UI with methods ready to use

---
+++ Assets/Interface/MainButtonsController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Assets.Interface
+{
+    public class MainButtonsController : MonoBehaviour {
+
+        // Use this for initialization
+        void Start () {
+		
+        }
+	
+        // Update is called once per frame
+        void Update () {
+		
+        }
+
+        public void MenuButtonClick()
+        {
+
+        }
+
+        public void ShopButtonClick()
+        {
+
+        }
+
+        public void MinigameButtonClick()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Interface/MainGaugesController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Interface
+{
+    public class MainGaugesController : MonoBehaviour
+    {
+        [SerializeField]
+        private Text _energy,_hunger,_knowledge;
+
+        private string EnergyGauge
+        {
+            set { _energy.text = "Energia " + value; }
+        }
+        private string HungerGauge
+        {
+            set { _energy.text = "Głód " + value; }
+        }
+        private string KnowledgeGauge
+        {
+            set { _energy.text = "Wiedza " + value; }
+        }
+
+        // Use this for initialization
+        void Start () {
+		
+        }
+	
+        // Update is called once per frame
+        void Update () {
+		
+        }
+    }
+}
