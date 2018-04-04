bfefec1bf54e61088fe0d3e434121f322de2e7db
CadoganPL <s160884@student.pg.gda.pl>
2018-04-04 10:19:43 +0200

    InitialCommit with timer,progressbar and event manager. ProgressBar.cs and Timer.cs lack UI update methods' implementation - waiting for interface design of minigames.

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+
+//Code source: https://unity3d.com/learn/tutorials/topics/scripting/events-creating-simple-messaging-system
+
+
+ //I left eventName as a string instead of creating an enum or sth like that to let it be flexible and simple
+ // without adding new enums during prototyping - after prototype it should be replaced with enum "EventType" or sth like this
+ // but be aware of fact ur able to accidentially create different events by a stupid mistake
+ // like "event1" and "evetn1" - simple misspelling leads to two different events
+public class EventManager : MonoBehaviour
+{
+
+    private Dictionary<string, UnityEvent> eventDictionary;
+
+    private static EventManager eventManager;
+
+    public static EventManager instance
+    {
+        get
+        {
+            if (!eventManager)
+            {
+                eventManager = FindObjectOfType(typeof(EventManager)) as EventManager;
+
+                if (!eventManager)
+                {
+                    Debug.LogError("There needs to be one active EventManger script on a GameObject in your scene.");
+                }
+                else
+                {
+                    eventManager.Init();
+                }
+            }
+
+            return eventManager;
+        }
+    }
+
+    void Init()
+    {
+        if (eventDictionary == null)
+        {
+            eventDictionary = new Dictionary<string, UnityEvent>();
+        }
+    }
+
+
+
+    public static void StartListening(string eventName, UnityAction listener)
+    {
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.AddListener(listener);
+        }
+        else
+        {
+            thisEvent = new UnityEvent();
+            thisEvent.AddListener(listener);
+            instance.eventDictionary.Add(eventName, thisEvent);
+        }
+    }
+
+    public static void StopListening(string eventName, UnityAction listener)
+    {
+        if (eventManager == null) return;
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public static void TriggerEvent(string eventName)
+    {
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(EventManager))]
+public class ProgressBar : MonoBehaviour {
+
+
+    [SerializeField]
+    GameObject bar; // blank gameobject, it is meant to hold reference to actuall bar in the UI.
+    // Use this for initialization
+    private float progress = 0.0f;
+    private float progressIncrease = 10.0f;
+    private float progressDecrease = 10.0f;
+    const float maxProgress = 100.0f;
+
+    private UnityAction increaseProgressDelegate; // UnityAction is a delegate defined by Unity to use it with event system etc.
+    private UnityAction decreseProgressDelegate; // instead of C#'s delegates.
+
+    void Awake () {
+        increaseProgressDelegate += IncreaseProgress;
+        decreseProgressDelegate += DecreaseProgress;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ModifyProgressBar();
+	}
+
+    private void DecreaseProgress()
+    {
+        progress -= progressDecrease; 
+        progress = progress < 0 ? 0 : progress; //ensuring progress is always equal or above 0.
+    }
+    private void IncreaseProgress()
+    {
+        progress += progressIncrease;
+        progress = progress > maxProgress ? maxProgress : progress; // ensuring prograss is always equal or below maxProgress constant.
+    }
+
+    void ModifyProgressBar()
+    {
+        //This method is supposed to change values of progress bars etc.,
+        //however there's still not any UI for minigames, so i leave it blank for later implementation.
+    }
+}
