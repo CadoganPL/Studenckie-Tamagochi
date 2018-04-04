9efc2d83b2b6670c198b871124d90166060bf705
Michal Przekop <przekop.michal@gmail.com>
2018-04-05 00:37:58 +0200

    MinigameUI
    Simple UI containing Progress bar and timer. Drag and drop.

---
+++ Assets/Scripts/MinigameUI/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.A))
+	    {
+	        EventManager.TriggerEvent("IncreaseProgressBar");
+	    }
+	    if (Input.GetKeyDown(KeyCode.D))
+	    {
+	        EventManager.TriggerEvent("DecreaseProgressBar");
+        }
+	}
+}
